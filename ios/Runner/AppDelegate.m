a6643ed5d9a500233ab197f0423c88b35ec662e0
wuliangcheng <wuliangcheng@126.com>
2018-05-03 19:32:01 +0800

    add IOS support.

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -1,11 +1,28 @@
 #include "AppDelegate.h"
 #include "GeneratedPluginRegistrant.h"
 
+#import <Flutter/Flutter.h>
+
+@import Hello;
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
   [GeneratedPluginRegistrant registerWithRegistry:self];
   // Override point for customization after application launch.
+    
+    FlutterViewController* controler = (FlutterViewController*) self.window.rootViewController;
+    FlutterMethodChannel* goChannel = [
+    FlutterMethodChannel methodChannelWithName:@"com.xiaoshike.io/hello" binaryMessenger:controler
+                                        ];
+    [
+     goChannel setMethodCallHandler:^(FlutterMethodCall * _Nonnull call, FlutterResult  _Nonnull result) {
+         //TODO
+         result(HelloGreetings(@"IOS,Go 2018!"));
+     }
+    ];
+    
+    
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
